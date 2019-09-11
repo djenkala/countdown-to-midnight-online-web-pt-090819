1ec1a34ef76e36c59d72e1d944607084c4ec02cd
Daniel Jenkala <djenkala@yahoo.com>
2019-09-11 21:06:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,5 @@
-counter = 10
-
-def countdown
-  until counter == 0
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
     sleep(1)
